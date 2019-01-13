d98bacda3b2bfccd7f4dcc21b1653e359c0ce667
Cory Steers <cory.steers.gmu9@statefarm.com>
2019-01-13 13:47:53 -0600

    switched underscores to strikethrough so the blank words would take the same space as the real word, and unhid punctuation

--- Memory/MemoryViewController.swift
+++ Memory/MemoryViewController.swift
@@ -13,6 +13,16 @@ class MemoryViewController: UIViewController {
 
     var item: MemoryItem!
     var blankCounter = 0
+    let visibleText: [NSAttributedString.Key: Any] = [
+        .font: UIFont(name: "Georgia", size: 28)!,
+        .foregroundColor: UIColor.black
+    ]
+    let invisibleText: [NSAttributedString.Key: Any] = [
+        .font: UIFont(name: "Georgia", size: 28)!,
+        .foregroundColor: UIColor.clear,
+        .strikethroughStyle: 1,
+        .strikethroughColor: UIColor.black
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,17 +36,33 @@ class MemoryViewController: UIViewController {
 
     func showText() {
         let words = item.text.components(separatedBy: " ")
-        var output = ""
+        let output = NSMutableAttributedString()
+        let space = NSAttributedString(string: " ", attributes: visibleText)
 
         for (index, word) in words.enumerated() {
+            let attributedWord: NSAttributedString
+            var attributedPunctuation: NSAttributedString?
             if index < blankCounter {
-                output += "\(word) "
+                attributedWord = NSAttributedString(string: word, attributes: visibleText)
             } else {
-                let blanks = String(repeating: "_", count: word.count)
-                output += "\(blanks) "
+                var strippedWord = word
+                var punctuation: String?
+
+                if ".,".contains(word.last!) {
+                    punctuation = String(strippedWord.removeLast())
+                }
+                attributedWord = NSAttributedString(string: strippedWord, attributes: invisibleText)
+                if let symbol = punctuation {
+                    attributedPunctuation = NSAttributedString(string: symbol, attributes: visibleText)
+                }
             }
+            output.append(attributedWord)
+            if let ap = attributedPunctuation {
+                output.append(ap)
+            }
+            output.append(space)
         }
-        textView.text = output
+        textView.attributedText = output
     }
 
     @objc func wordsTapped() {
